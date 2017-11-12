23a80fe23ce799ee98783d3083fd36a6896e1552
kaang <slydeller@gmail.com>
2017-11-12 19:24:18 +0200

    Add setting to Heroku

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
+
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -52,8 +55,8 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Stated on port 3000')
+app.listen(port, () => {
+    console.log('Stated on port ' + port)
 });
 
 module.exports = {app};
